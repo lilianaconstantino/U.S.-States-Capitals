2f465df9c8831e01715880a5255bf293e797063f
Cristiain Reyes <cris@Cristiains-MacBook-Pro.local>
2025-08-27 13:38:21 -0700

    Styled the modal, also refined the design for the home nav bar.

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Modal.module.css';
+import Button from '../Button/Button';
 
 function Modal({ isOpen, onClose, selectedState }) {
   if (!isOpen || !selectedState) {
@@ -7,21 +8,23 @@ function Modal({ isOpen, onClose, selectedState }) {
   }
 
   return (
-    <div className={styles.modalBackdrop}>
-      <div className={styles.modalContent}>
-        <h2>{selectedState.name}</h2>
-        
-        <p>Capital: {selectedState.capital}</p>
-        <p>Abbreviation: {selectedState.postalAbreviation}</p>
-        <p>Population: {selectedState.population?.toLocaleString()}</p>
-        <p>Largest City: {selectedState.largestCity}</p>  
-        <p>Total Area: {selectedState.totalAreaSquareMiles?.toLocaleString()} sq miles</p>
-        <p>Established: {selectedState.established}</p>
-        
-        <button onClick={onClose}>Close</button>
-      </div>
+  <div className={styles.modalBackdrop}>
+    <div className={styles.modalContent}>
+      <h2 className={styles.stateTitle}>{selectedState.name}</h2>
+
+       <div className={styles.stateContent}>
+      <p><span className={styles.label}>Capital:</span> {selectedState.capital}</p>
+      <p><span className={styles.label}>Abbreviation:</span> {selectedState.postalAbreviation}</p>
+      <p><span className={styles.label}>Population:</span> {selectedState.population?.toLocaleString()}</p>
+      <p><span className={styles.label}>Largest City:</span> {selectedState.largestCity}</p>  
+      <p><span className={styles.label}>Total Area:</span> {selectedState.totalAreaSquareMiles?.toLocaleString()} sq miles</p>
+      <p><span className={styles.label}>Established:</span> {selectedState.established}</p>
+       </div>
+      
+      <Button className={styles.closeButton} onClick={onClose}>Close</Button>
     </div>
-  );
+  </div>
+);
 }
 
 export default Modal;
