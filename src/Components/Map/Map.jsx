3a23d34ea56a96034493cc0c209bfbf5feaa60de
Liliana <constantinolilianam@gmail.com>
2025-10-23 23:34:46 -0700

    Added state abreviations to map and state a capital labels to results map

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { USAMap } from "@mirawision/usa-map-react";
 import styles from "./Map.module.css";
 
-function Map({ onStateClick, clickedStates = {}, results = [], showLabels = false }) {
+function Map({ onStateClick, clickedStates = {}, results = [], showLabels = false, showAbbreviations = false }) {
   const [stateData, setStateData] = useState([]);
 
   useEffect(() => {
@@ -42,56 +42,56 @@ function Map({ onStateClick, clickedStates = {}, results = [], showLabels = fals
 
   // Use your pre-defined percentages for capital positions
   const capitalPositions = {
-    AL: { top: "66%", left: "67%" },
-    AK: { top: "90%", left: "5%" },
-    AZ: { top: "60%", left: "22%" },
-    AR: { top: "62%", left: "56%" },
-    CA: { top: "47%", left: "9%" },
-    CO: { top: "46%", left: "34%" },
-    CT: { top: "100%", left: "79%" },
-    DE: { top: "90%", left: "75%" },
-    FL: { top: "86%", left: "79%" },
-    GA: { top: "70%", left: "74%" },
-    HI: { top: "95%", left: "12%" },
-    ID: { top: "26%", left: "21%" },
-    IL: { top: "42%", left: "61%" },
-    IN: { top: "43%", left: "67%" },
-    IA: { top: "37%", left: "54%" },
-    KS: { top: "49%", left: "46%" },
-    KY: { top: "100%", left: "57%" },
-    LA: { top: "78%", left: "58%" },
-    ME: { top: "10%", left: "88%" },
-    MD: { top: "100%", left: "71%" },
-    MA: { top: "5%", left: "78%" },
-    MI: { top: "32%", left: "69%" },
-    MN: { top: "20%", left: "53%" },
-    MS: { top: "70%", left: "62%" },
-    MO: { top: "50%", left: "56%" },
-    MT: { top: "18%", left: "30%" },
-    NE: { top: "39%", left: "43%" },
-    NV: { top: "40%", left: "15%" },
-    NH: { top: "10%", left: "83%" },
-    NJ: { top: "80%", left: "74%" },
-    NM: { top: "62%", left: "32%" },
-    NY: { top: "28%", left: "84%" },
-    NC: { top: "55%", left: "80%" },
-    ND: { top: "18%", left: "43%" },
-    OH: { top: "41%", left: "72%" },
-    OK: { top: "60%", left: "48%" },
-    OR: { top: "22%", left: "12%" },
-    PA: { top: "37%", left: "80%" },
-    RI: { top: "10%", left: "61%" },
-    SC: { top: "62%", left: "78%" },
-    SD: { top: "28%", left: "43%" },
-    TN: { top: "57%", left: "68%" },
-    TX: { top: "75%", left: "43%" },
-    UT: { top: "43%", left: "24%" },
-    VT: { top: "20%", left: "80%" },
-    VA: { top: "48%", left: "81%" },
-    WA: { top: "10%", left: "15%" },
-    WV: { top: "46%", left: "76%" },
-    WI: { top: "27%", left: "60%" },
-    WY: { top: "32%", left: "31%" },
+    AL: { top: "60%", left: "67%" },
+    AK: { top: "69%", left: "13%" },
+    AZ: { top: "50%", left: "21%" },
+    AR: { top: "52%", left: "57%" },
+    CA: { top: "40%", left: "8%" },
+    CO: { top: "40%", left: "34%" },
+    CT: { top: "28%", left: "92%" },
+    DE: { top: "35%", left: "90%" },
+    FL: { top: "73%", left: "80%" },
+    GA: { top: "60%", left: "74%" },
+    HI: { top: "80%", left: "29%" },
+    ID: { top: "23%", left: "21%" },
+    IL: { top: "35%", left: "61%" },
+    IN: { top: "35%", left: "67%" },
+    IA: { top: "30%", left: "54%" },
+    KS: { top: "41%", left: "45%" },
+    KY: { top: "43%", left: "70%" },
+    LA: { top: "65%", left: "58%" },
+    ME: { top: "12%", left: "93%" },
+    MD: { top: "40%", left: "88%" },
+    MA: { top: "23%", left: "96%" },
+    MI: { top: "25%", left: "69%" },
+    MN: { top: "15%", left: "53%" },
+    MS: { top: "60%", left: "62%" },
+    MO: { top: "40%", left: "56%" },
+    MT: { top: "15%", left: "30%" },
+    NE: { top: "32%", left: "43%" },
+    NV: { top: "33%", left: "14%" },
+    NH: { top: "20%", left: "95%" },
+    NJ: { top: "30%", left: "90%" },
+    NM: { top: "53%", left: "31%" },
+    NY: { top: "22%", left: "84%" },
+    NC: { top: "45%", left: "82%" },
+    ND: { top: "14%", left: "43%" },
+    OH: { top: "35%", left: "72%" },
+    OK: { top: "50%", left: "48%" },
+    OR: { top: "18%", left: "10%" },
+    PA: { top: "30%", left: "80%" },
+    RI: { top: "26%", left: "93%" },
+    SC: { top: "52%", left: "78%" },
+    SD: { top: "23%", left: "43%" },
+    TN: { top: "48%", left: "68%" },
+    TX: { top: "63%", left: "43%" },
+    UT: { top: "35%", left: "23%" },
+    VT: { top: "12%", left: "87%" },
+    VA: { top: "40%", left: "81%" },
+    WA: { top: "8%", left: "13%" },
+    WV: { top: "40%", left: "76%" },
+    WI: { top: "20%", left: "60%" },
+    WY: { top: "25%", left: "31%" },
   };
 
   const getStateSettings = () => {
@@ -125,37 +125,60 @@ function Map({ onStateClick, clickedStates = {}, results = [], showLabels = fals
 
   return (
     <div className={styles.mapWrapper}>
-      <USAMap className={styles.map} customStates={getStateSettings()} />
+      <div className={styles.mapContainer}>
+        <USAMap className={styles.map} customStates={getStateSettings()} />
+        </div>
 
-      {/* Labels overlay only when showing results */}
-      {showLabels &&
-        results.map(({ state, capital, correct }) => {
-          const abbrev = stateData.find((s) => s.name === state)?.postalAbreviation;
-          const pos = capitalPositions[abbrev];
-          if (!pos) return null;
+  {/* Always show abbreviations during gameplay */}
+  {!showLabels &&
+    Object.entries(capitalPositions).map(([abbr, pos]) => (
+      <div
+        key={abbr}
+        style={{
+          position: "absolute",
+          top: pos.top,
+          left: pos.left,
+          transform: "translate(-50%, -50%)",
+          fontSize: "9px",
+          fontWeight: "bold",
+          color: "#333",
+          pointerEvents: "none",
+          textShadow: "0 0 2px #fff",
+        }}
+      >
+        {abbr}
+      </div>
+    ))}
 
-          return (
-            <div
-              key={state}
-              style={{
-                position: "absolute",
-                top: pos.top,
-                left: pos.left,
-                transform: "translate(-50%, -50%)",
-                textAlign: "center",
-                color: correct ? "green" : "red",
-                fontWeight: "bold",
-                pointerEvents: "none",
-                fontSize: "11px",
-                textShadow: "0 0 2px #fff",
-              }}
-            >
-              <div>{state}</div>
-              <div style={{ fontSize: "9px", fontWeight: "normal" }}>{capital}</div>
-            </div>
-          );
-        })}
-    </div>
+  {/* Show full names and capitals on results map */}
+  {showLabels &&
+    results.map(({ state, capital, correct }) => {
+      const abbrev = stateData.find((s) => s.name === state)?.postalAbreviation;
+      const pos = capitalPositions[abbrev];
+      if (!pos) return null;
+
+      return (
+        <div
+          key={state}
+          style={{
+            position: "absolute",
+            top: pos.top,
+            left: pos.left,
+            transform: "translate(-50%, -50%)",
+            textAlign: "center",
+            color: correct ? "green" : "red",
+            fontWeight: "bold",
+            pointerEvents: "none",
+            fontSize: "11px",
+            textShadow: "0 0 2px #fff",
+          }}
+        >
+          <div>{state}</div>
+          <div style={{ fontSize: "9px", fontWeight: "normal" }}>{capital}</div>
+        </div>
+      );
+    })}
+</div>
   );
 }
 
